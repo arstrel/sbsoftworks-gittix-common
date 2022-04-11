102d1cf255c369469113602e254b167e246edaef
Artem <arstrel@gmail.com>
2022-04-11 13:16:58 -0700

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -1,7 +1,6 @@
 import { Subjects } from './subjects';
-import { OrderStatus } from './types/order-status';
 
-export interface OrcerCancelledEvent {
+export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
